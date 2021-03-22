108a282d84459c47653d211e0de4bc4a8ffa7560
nsa0218 <nsa0218@noreply.github.com>
2021-03-23 00:46:17 +0300

    12. Ders sonu

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _productService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+           return Ok(_productService.Add(product));
+        }
+    }
+}
